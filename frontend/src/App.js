36eecd8183b749e0b9d7a0b60d8819af100f370c
unknown <ridamnagar11@gmail.com>
2022-05-27 11:33:57 +0530

    Added gtm code to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,17 @@ import Homepage from './Pages/Homepage';
 import {Route} from 'react-router-dom';
 import Chatpage from './Pages/Chatpage';
 import EmailVerify from './components/EmailVerify';
+import TagManager from 'react-gtm-module'
+
+  const tagManagerArgs = {
+  gtmId: 'GTM-NCHJ4VL'
+}
+TagManager.initialize(tagManagerArgs)
 function App() {
+  window.dataLayer.push({
+    event: 'pageview'
+  });
+
   return (
     <div className="App">
        <Route path="/" component={Homepage} exact>
